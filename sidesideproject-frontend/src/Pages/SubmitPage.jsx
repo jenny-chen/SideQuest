1840d97225dce4be92128888d8deb93bd768efc6
Michael Pu <michael.pu123@gmail.com>
2019-09-14 09:21:06 -0400

    Debug submit page

--- sidesideproject-frontend/src/Pages/SubmitPage.jsx
+++ sidesideproject-frontend/src/Pages/SubmitPage.jsx
@@ -7,43 +7,42 @@ export default class SubmitPage extends Component {
     }
 
     render() {
-
         return (
             <div>
                 <Navbar />
                 <div className='container mt-4'>
                     <form>
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Email address</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlInput1">Email address</label>
+                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                         </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Example select</label>
-                                <select class="form-control" id="exampleFormControlSelect1">
-                                  <option>1</option>
-                                  <option>2</option>
-                                  <option>3</option>
-                                  <option>4</option>
-                                  <option>5</option>
-                                </select>
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlSelect1">Example select</label>
+                            <select className="form-control" id="exampleFormControlSelect1">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect2">Example multiple select</label>
-                                <select multiple class="form-control" id="exampleFormControlSelect2">
-                                  <option>1</option>
-                                  <option>2</option>
-                                  <option>3</option>
-                                  <option>4</option>
-                                  <option>5</option>
-                                </select>
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlSelect2">Example multiple select</label>
+                            <select multiple className="form-control" id="exampleFormControlSelect2">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Example textarea</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <div className="form-group">
+                            <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
                     </form>
                 </div>
             </div>
-        )
+        );
     }
 }
