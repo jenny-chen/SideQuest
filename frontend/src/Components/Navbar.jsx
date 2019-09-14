334f581bd8b453275c13b0b44f45e8258822fb63
Michael Pu <michael.pu123@gmail.com>
2019-09-14 18:12:30 -0400

    update logo

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from '../icon.png';
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand" to='/'><img src={logo} height='50'/></Link>
+            <Link className="navbar-brand" to='/'><img className='m-2' src={logo} height='30'/></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
