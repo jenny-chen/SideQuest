4d28d7da507a56f35a58470b34548d6c84d17f8b
Michael Pu <michael.pu123@gmail.com>
2019-09-14 09:26:12 -0400

    Add submit page

--- sidesideproject-frontend/src/App.js
+++ sidesideproject-frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Switch } from 'react-router';
 import { Route } from 'react-router-dom';
 import SearchPage from './Pages/SeachPage';
-import SubmitPage from './Pages/SubmitPage'
+import SubmitPage from './Pages/SubmitPage';
 
 function App() {
     return (
         <div className="App">
             <Switch>
-                <Route exact path="/" component={SubmitPage} />
+                <Route exact path="/" component={SearchPage} />
+                <Route exact path="/submit" component={SubmitPage} />
             </Switch>
         </div>
     );
