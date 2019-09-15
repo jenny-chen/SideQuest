511dc7e3fc9d99a98843171b543291b146f133e5
Michael Pu <michael.pu123@gmail.com>
2019-09-14 23:48:03 -0400

    Bug fix

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Components/Navbar';
 import logo from '../logo.png';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
     return (
@@ -12,8 +13,8 @@ export default function HomePage() {
                     <h4>A place for people who don't know what side projects to make.</h4>
                 </div>
                 <div className='mt-5'>
-                    <a className='btn btn-secondary mr-3' href='/search'>Browse Projects</a>
-                    <a className='btn btn-secondary ml-3' href='/submit'>Submit Project</a>
+                    <Link className='btn btn-secondary mr-3' href='./search'>Browse Projects</Link>
+                    <Link className='btn btn-secondary ml-3' href='./submit'>Submit Project</Link>
                 </div>
             </div>
         </div>
